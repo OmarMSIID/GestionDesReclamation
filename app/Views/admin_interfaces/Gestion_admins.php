fa6f9d65d6b45411e3444f4b69d4736926285331
chammAyoub <ayoub.chammakh.23@edu.uiz.ac.ma>
2024-10-31 17:54:34 +0000

    Ajouter l'interface pour l'ajout d'un administrateur

---
+++ app/Views/admin_interfaces/Gestion_admins.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gestion_admins</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body style="background-color:#dfe9f6;">
+    <div class="container d-flex justify-content-center align-items-center" style="min-height: 100vh;">
+        <div class="card shadow p-4" style="width: 100%; max-width: 400px;">
+            <h3 class="text-center mb-4">Ajouter Admin</h3>
+            <form action="<?= base_url('ajouteAdmin') ?>" method="post">
+                <div class="form-group">
+                    <label for="email">E-mail</label>
+                    <input type="email" class="form-control" id="email" name="email" required>
+                </div>
+                <div class="form-group">
+                    <label for="nom_utilisateur">Nom d'utilisateur</label>
+                    <input type="text" class="form-control" id="nom_utilisateur" name="nom_utilisateur" required>
+                </div>
+                <div class="form-group">
+                    <label for="mot_de_passe">Mot de passe</label>
+                    <input type="password" class="form-control" id="mot_de_passe" name="mot_de_passe" required>
+                </div>
+                <div class="form-group">
+                    <button id="ajouter" name="ajouter" type="submit" class="btn btn-primary btn-block">Ajouter</button>
+                </div>
+            </form>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.4.4/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
