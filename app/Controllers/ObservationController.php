77b30a4a330fb5e16f355591cee5cb09d18e5341
chammAyoub <ayoub.chammakh.23@edu.uiz.ac.ma>
2024-11-01 01:17:38 +0000

    Modifier le controleur de l'Observation : - creation de la mathode ajouterObservation() pour ajouter une observation.

--- app/Controllers/ObservationController.php
+++ app/Controllers/ObservationController.php
@@ -2,10 +2,33 @@
 
 namespace App\Controllers;
 
+use App\Models\ObservationModel;
+use App\Entities\Observation;
+use DateTime;
+
 class ObservationController extends BaseController
 {
     public function index(): string
     {
         return view('user_interfaces/Soumettre_Forms/Soumettre_Observation');
+    }
+
+    public function ajouterObservation()
+    {
+        $observationModel = new ObservationModel();
+        $observation = new Observation();
+
+        $observation->nom_utilisateur = $this->request->getPost('nom_utilisateur');
+        $observation->email = $this->request->getPost('email');
+        $observation->setDate(new DateTime());
+        $observation->sujet = $this->request->getPost('sujet');
+        $observation->description = $this->request->getPost('description');
+        
+        // Sauvegarder dans la base de données
+        if ($observationModel->save($observation)) {
+            return view('user_interfaces/Soumettre_Forms/Soumettre_Observation', ['showSuccessModal' => true]);
+        } else {
+            return redirect()->back()->with('error', "Échec de l'ajout d'une observation" . implode(', ', $observationModel->errors()));
+        }
     }
 }
