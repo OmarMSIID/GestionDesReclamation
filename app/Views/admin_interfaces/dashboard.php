b6460cbd2a56af20140894ba86464625d96c685e
chammAyoub <ayoub.chammakh.23@edu.uiz.ac.ma>
2024-11-07 19:46:52 +0000

    L'joute du tableau de bord pour les administrateurs, accessible dans les pages : - Gestion_Reclamation. - Gestion_Observation. - Gestion_Suggestion. - Gestion_Utilisateur.

---
+++ app/Views/admin_interfaces/dashboard.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tableau de Bord Admin</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <style>
+        .sidebar {
+            background-color: #fff;
+            height: 100vh;
+            position: fixed;
+            width: 17%;
+            padding: 0;
+            border-right: 1px solid #dee2e6;
+        }
+        .sidebar h4 {
+            padding-top: 20px;
+            padding-bottom: 30px;
+            font-weight: bold;
+        }
+        .nav-link {
+            color: #495057;
+            font-size: 16px;
+            padding: 20px;
+            display: flex;
+            align-items: center;
+            width: 100%;
+        }
+        .nav-link.active {
+            background-color: #e2e6ea;
+            color: #007bff;
+            font-weight: bold;
+        }
+        .nav-link i {
+            margin-right: 10px;
+            font-size: 18px;
+        }
+        .nav-link:hover {
+            color: #007bff;
+            text-decoration: none;
+        }
+        .text-danger {
+            color: #dc3545 !important;
+            font-weight: bold;
+        }
+        .logout{
+            position: absolute;
+            bottom: 0;
+            color: whitesmoke;
+            background-color: rgb(194, 58, 58);
+            width: 100%;
+            padding:2px 20px ;
+            text-align: center;
+            align-items: center;
+            justify-content: center;
+        }
+        .logout:hover{
+            background-color: rgb(194, 58, 58);
+        }
+        .logout_a:hover{
+            color:whitesmoke;
+        }
+        .logout_a{
+            color: whitesmoke;
+        }
+    </style>
+</head>
+<body style="background-color: #dfe9f5;">
+    <div class="container-fluid">
+        <div class="row">
+            <nav class="  d-md-block sidebar">
+                <div class="text-center">
+                    <h4>Tableau de Bord</h4>
+                    <ul class="nav flex-column mt-5 text-center">
+                        <li class="nav-item text-center">
+                            <a class="nav-link text-center ml-2" href="<?= base_url('Liste_Reclamations') ?>">
+                                <i class="fas fa-exclamation-circle fa-fw"></i> Réclamations
+                            </a>
+                        </li>
+                        <li class="nav-item text-center">
+                            <a class="nav-link text-center ml-2" href="<?= base_url('Liste_Observations') ?>">
+                                <i class="fas fa-eye fa-fw"></i> Observations
+                            </a>
+                        </li>
+                        <li class="nav-item text-center">
+                            <a class="nav-link text-center ml-2" href="<?= base_url('Liste_Suggestions') ?>">
+                                <i class="fas fa-lightbulb fa-fw"></i> Suggestions
+                            </a>
+                        </li>
+                        <li class="nav-item text-center">
+                            <a class="nav-link text-center ml-2" href="<?= base_url('Liste_Utilisateurs') ?>">
+                                <i class="fas fa-users fa-fw"></i> Utilisateurs
+                            </a>
+                        </li>
+                        <li class="nav-item logout text-center">
+                            <a class="nav-link btn-btn danger logout_a text-center" href="<?= base_url('admin/deconnecter') ?>">
+                                <i class="fas fa-sign-out-alt fa-fw"></i> Déconnecter
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
+                <div class="content">
+                    <?= $this->renderSection('content') ?>
+                </div>
+            </main>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
