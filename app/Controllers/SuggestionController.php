7e6d16794ed6cf4746649e67e722d035b650ef4e
chammAyoub <ayoub.chammakh.23@edu.uiz.ac.ma>
2024-11-08 00:19:51 +0000

    Modifier le contrôleur SuggestionController.php en ajoutant deux méthodes : - afficher_suggestions( ) qui récupère les informations des suggestions depuis la base de données et les envoie à l'interface Gestion_Suggestion.php. - supprimer_suggestion( ) qui prend en paramètre l'ID d'une suggestion et la supprime.

--- app/Controllers/SuggestionController.php
+++ app/Controllers/SuggestionController.php
@@ -31,4 +31,22 @@ class SuggestionController extends BaseController
             return redirect()->back()->with('error', "Échec de l'ajout d'une suggestion" . implode(', ', $suggestionModel->errors()));
         }
     }
+
+    public function afficher_suggestions()
+    {
+        $suggestionModel = new SuggestionModel();
+        $data['suggestions'] = $suggestionModel->findAll();
+        
+        return view('admin_interfaces/Gestion_Suggestion', $data);
+    }
+
+    public function supprimer_suggestion($id)
+    {
+        $suggestionModel = new SuggestionModel();
+        if ($suggestionModel->delete($id)) {
+            return redirect()->to('/Liste_Suggestions')->with('success', 'Suggestions supprimée.');
+        } else {
+            return redirect()->to('/Liste_Suggestions')->with('error', 'Échec de la suppression.');
+        }
+    }
 }
